60e7ff6cd34f85344a2f8330976fdb6ee9f2cb75
izaxveta <izavetha@gmail.com>
2018-07-30 18:30:51 -0500

    Renames issues controller method

--- app/controllers/api/v1/issues_controller.rb
+++ app/controllers/api/v1/issues_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::IssuesController < ApplicationController
   def index
-    issues = handle_queries(params)
+    issues = handle_issues_request(params)
     render status: 200, json: issues
   end
 
   private
-    def handle_queries(params)
+    def handle_issues_request(params)
       return Issue.find_by_summary(params[:issue_summary]) if params[:issue_summary]
       return User.reported_issues(params[:reporter_name]) if params[:reporter_name]
       return User.assigned_issues(params[:assignee_name]) if params[:assignee_name]
