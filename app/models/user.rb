bf1dacf4e78ef0678a7d8790ad1dff79d206316b
izaxveta <izavetha@gmail.com>
2018-07-30 20:25:23 -0500

    Adds validations for uniqueness of email, eliminates whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :name, :email, presence: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
   has_many :reported_issues, class_name: 'Issue', foreign_key: 'reporter_id'
   has_many :assigned_issues, class_name: 'Issue', foreign_key: 'assignee_id'
@@ -11,5 +12,4 @@ class User < ApplicationRecord
   def self.assigned_issues(user_name)
     find_by(name: user_name).assigned_issues
   end
-
 end
