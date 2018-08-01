ac171655f801a7f3aa7dc57729570d881a65e9f6
izaxveta <izavetha@gmail.com>
2018-07-31 19:05:08 -0500

    Updates issue serializer to format timestamps and render reporter and assignee based off relationship

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -1,3 +1,14 @@
 class IssueSerializer < ActiveModel::Serializer
-  attributes :id, :summary, :status, :reporter, :assignee
+  attributes :id, :summary, :status, :created_at, :updated_at
+
+  belongs_to :reporter
+  belongs_to :assignee
+
+  def created_at
+    object.created_at.strftime("%m/%d/%Y %I:%M%p")
+  end
+
+  def updated_at
+    object.updated_at.strftime("%m/%d/%Y %I:%M%p")
+  end
 end
